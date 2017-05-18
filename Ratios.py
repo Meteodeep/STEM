56954788f8f9dffb1c410631c7ca5c28fca78e48
Meteodeep <dobg1.91@gmail.com>
2017-05-18 16:00:43 +0100

    Create Ratios.py

---
+++ Ratios.py
@@ -0,0 +1,15 @@
+#A program for assiting in the calculation of Ratios!!
+print("Welcome to the Ratio Calculator")
+first=(int(input("Please enter the Amount of Money you are sharing/dividing")))
+print("In this program we will use the following variables in our Ratio; x:y")
+second=(int(input("Please enter the number for 'x'")))
+third=(int(input("Please enter the number for 'y'")))
+fourth = second+third
+fifth = first/fourth
+sixth = second*fifth
+seventh = third*fifth
+sixth = (str(sixth))
+seventh = (str(seventh))
+print("Your Ratio is.........")
+print(sixth + ":" + seventh)
+print("Thank You for using this program!!")
