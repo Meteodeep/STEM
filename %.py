9c282110092b84c3b2a69e346c5e05bc8dfb1aa7
Meteodeep <dobg1.91@gmail.com>
2017-06-12 14:11:58 +0100

    Create %.py

---
+++ %.py
@@ -0,0 +1,9 @@
+number1=int(input("Please enter the number you would like to find the percentage of"))
+
+number2=int(input("Now enter the percentage, e.g. 50%"))
+            
+answer=(number1/100)
+answer=(answer*number2)
+
+
+print(answer) 
