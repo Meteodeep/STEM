60f4b404e1019769d6af4b2ffe862fb9c8b9636a
Meteodeep <dobg1.91@gmail.com>
2017-12-13 10:17:23 +0000

    Tax

---
+++ Tax.py
@@ -0,0 +1,5 @@
+numberone=int(input("Please enter your Sub-Total"))
+numbertwo=int(input("Please enter the percentage of tax/dividends without the % sign"))
+answer=(numberone/100)
+answer=(answer*numbertwo)
+print(answer)
